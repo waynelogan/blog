5cf804a1b44b134e7001fbd570bf36b590124c1f
waynelogan <loganamollo@gmail.com>
2024-01-16 18:38:00 +0300

    updated button rendering

--- app/components/ThemeButton.tsx
+++ app/components/ThemeButton.tsx
@@ -1,9 +1,16 @@
 'use client'
 
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export default function ThemeButton() {
     const { setTheme, resolvedTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+    useEffect( () => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) return null
 
     return (
         <button onClick={() => setTheme( resolvedTheme === 'dark' ? 'light' : 'dark' ) } >
